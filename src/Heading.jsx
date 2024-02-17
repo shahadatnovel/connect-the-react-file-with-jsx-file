573693fe06b524c0a83a6f7bc8ec41441d4284ab
Shahadat hossin novel <154046241+shahadatnovel@users.noreply.github.com>
2024-02-17 11:37:27 +0600

    Add files via upload

---
+++ src/Heading.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+function Heading(){
+    return(
+      <div>
+       <h1>hello world</h1>
+       <p>this is my 1st react</p>
+       </div>
+    );
+}
+
+export default Heading;
---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Heading from ' ./Heading';
+  
+
+ReactDOM.render(
+   <>
+  <Heading />
+  </>,
+
+  document.getElementById("root")
+);
+
+  
+ 
+
